a6cb4f6a175c2235663cf0f83e913ac9b0ec67cd
Kirill Demidov <kademidov@gmail.com>
2024-11-08 13:28:48 +0300

    Созданы внутренние страницы. Сверстана страница faq.

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,7 +1,67 @@
 $(document).ready(function() {
 
-  $('.js-burger').on('click', function() {
-    $('.js-menu').show();
+  let $menu = $('.js-menu');
+  // let isOpen = false;
+
+  $('.js-burger').on('click', function () {
+    // if ( isOpen ) {
+    //   $menu.slideUp();
+    //   isOpen = false;
+    // } else {
+    //   $menu.slideDown();
+    //   isOpen = true;
+    // }
+    $menu.slideToggle();
+
+  });
+
+
+  let prevAccordionBtn;
+  let openClass = 'open';
+
+  $('.js-accordion-btn').on('click', function() {
+    if (this === prevAccordionBtn) {
+      $(this).toggleClass(openClass)
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $(prevAccordionBtn).removeClass(openClass);
+    $(prevAccordionBtn).next().slideUp();
+
+    $(this).addClass(openClass);
+    $(this).next().slideDown();
+
+    prevAccordionBtn = this;
+  });
+
+
+  let $servicesHeading = $('.services_heading');
+
+  $servicesHeading.each(function() {
+    console.log( $(this).text() );
   });
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
